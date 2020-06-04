9ed2eb119bb58c4c1d6813da0d2fe2bda56e16b7
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-04 20:04:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
-# your code goes here
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene_points
+
+  def initialize(name, bank_account = 25, happiness = 8, hygiene_points = 8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = 8
+    @hygiene_points = 8
+  end
+
+  def happiness=(points)
+    if points >= 0 && 10 >= points
+      @happiness = points
+    end
+  end
+
+
+end
+
+
+
+
+end
